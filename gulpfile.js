fe0f941d500ef9b37dba31574475a217dc23d6bc
Jojo <jojoyuji@gmail.com>
2014-10-20 08:52:01 -0300

    added browser-sync workflow + angularjs spa for docs

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,28 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var concatCss = require('gulp-concat-css');
+var browserSync = require('browser-sync');
 
 gulp.task('concat-js', function() {
   gulp.src(['./assets/js/*.js', './src/**/*.js'])
-    .pipe(concat('hug-ui.js'))
-    .pipe(gulp.dest('./dist/'));
+  .pipe(concat('hug-ui.js'))
+  .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('concat-css', function() {
   gulp.src(['./assets/icons/style.css', './src/**/*.css'])
-    .pipe(concatCss('hug-ui.css'))
-    .pipe(gulp.dest('./dist/'));
+  .pipe(concatCss('hug-ui.css'))
+  .pipe(gulp.dest('./dist/'));
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: './'
+    }
+  });
 });
 
 gulp.task('concat', ['concat-js', 'concat-css']);
+gulp.task('default', ['browser-sync']);
